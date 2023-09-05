8f0d544fa79c0f8f4bab102a0e5ff74f126a8801
ozo-vehe <johnnie.vehe@gmail.com>
2023-09-05 23:13:34 +0100

    added the star assignment

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Grid from "./Grid";
 import Counter from "./Counter";
+import StarRating from "./StarRating";
 import "./App.css";
 
 // solve the css, range utility and Grid component
@@ -7,11 +8,36 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h2>Counter Assignment</h2>
+      <h1>Counter Assignment</h1>
       <Counter />
-      <h2> Star Assignment </h2>
 
-      <h2>Grid Assignment</h2>
+      <h1> Star Assignment </h1>
+      <div>
+        <section>
+          <h2>Winter&apos;s Orbit</h2>
+          <p>Rating: 5 / 5</p>
+          <StarRating rating={5} />
+        </section>
+        <section>
+          <h2>Dial D for Deadman</h2>
+          <p>Rating: 4 / 5</p>
+          <StarRating rating={4} />
+        </section>
+        <section>
+          <h2>Words And Things</h2>
+          <p>Rating: 0 / 5</p>
+          <StarRating rating={0} />
+        </section>
+        <section>
+          <h2>The Galaxy, And The Ground Within</h2>
+          <p>Rating: 5 / 5</p>
+          <StarRating rating={5} />
+        </section>
+      </div>
+
+      {/* Line break */}
+
+      <h1>Grid Assignment</h1>
       <Grid numRows={8} numCols={6} />
     </>
   );
